4dcf5ceafb6d395060a74d58a94f44eefe11dff2
DongJinLee <dlehdwls21@naver.com>
2023-10-17 18:38:30 +0900

    Init : DIG-3 개발 환경 구축

---
+++ src/main/java/com/ogjg/daitgym/DaItGymApplication.java
@@ -0,0 +1,13 @@
+package com.ogjg.daitgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaItGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaItGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogjg/daitgym/DaItGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogjg.daitgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaItGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
