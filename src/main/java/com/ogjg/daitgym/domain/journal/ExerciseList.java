2ba3edc0fe141961a9fc0740e6676aa347769b5f
DongJinLee <dlehdwls21@naver.com>
2023-10-26 12:21:37 +0900

    Feat : DIG-12 초기 엔티티 설정 (운동목록) 추가
    - 운동일지 엔티티 변경
    - 운동기록 엔티티 변경
    - 운동목록 엔티티 생성

--- src/main/java/com/ogjg/daitgym/domain/journal/ExerciseList.java
+++ src/main/java/com/ogjg/daitgym/domain/journal/ExerciseList.java
@@ -1,9 +1,12 @@
 package com.ogjg.daitgym.domain.journal;
 
 import com.ogjg.daitgym.domain.BaseEntity;
+import com.ogjg.daitgym.domain.Exercise;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.sql.Time;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -12,23 +15,21 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 @Entity
 @NoArgsConstructor(access = PROTECTED)
-public class ExerciseHistory extends BaseEntity {
+public class ExerciseList extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "exercise_history_id")
+    @Column(name = "exercise_list_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "exercise_list_id")
-    private ExerciseList exerciseList;
+    @JoinColumn(name = "journal_id")
+    private ExerciseJournal exerciseJournal;
 
-    private int setCount;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
 
-    private int weight;
-
-    private int repetition_count;
-
-    private boolean exercise_status;
+    private Time restTime;
 
 }
