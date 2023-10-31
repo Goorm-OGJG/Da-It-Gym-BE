ea04833b6059da0675a6df55dfa276a7b58d0c39
ByeongGyu An <devafox62@gmail.com>
2023-10-31 11:53:01 +0900

    Feat : DIG-31 모든 사용자 루틴 목록 조회 기능 구현 - 모든 사용자들에 대한 루틴 목록을 조회 API - Slice 방식을 사용하여 구현

--- src/main/java/com/ogjg/daitgym/routine/controller/RoutineController.java
+++ src/main/java/com/ogjg/daitgym/routine/controller/RoutineController.java
@@ -1,17 +1,29 @@
 package com.ogjg.daitgym.routine.controller;
 
+import com.ogjg.daitgym.common.exception.ErrorCode;
+import com.ogjg.daitgym.common.response.ApiResponse;
+import com.ogjg.daitgym.routine.dto.RoutineListResponseDto;
 import com.ogjg.daitgym.routine.service.RoutineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/routine")
+@RequestMapping("/api/routines")
 public class RoutineController {
 
     private final RoutineService routineService;
 
+    @GetMapping
+    public ApiResponse<?> getRoutines(Pageable pageable) {
+        RoutineListResponseDto routines = routineService.getRoutines(pageable);
+
+        return new ApiResponse<>(ErrorCode.SUCCESS, routines);
+    }
 }
--- src/main/java/com/ogjg/daitgym/routine/service/RoutineService.java
+++ src/main/java/com/ogjg/daitgym/routine/service/RoutineService.java
@@ -1,9 +1,17 @@
 package com.ogjg.daitgym.routine.service;
 
+import com.ogjg.daitgym.comment.routine.exception.NotFoundRoutine;
+import com.ogjg.daitgym.domain.routine.Routine;
+import com.ogjg.daitgym.routine.dto.RoutineDto;
+import com.ogjg.daitgym.routine.dto.RoutineListResponseDto;
 import com.ogjg.daitgym.routine.repository.RoutineRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,4 +20,34 @@ public class RoutineService {
 
     private final RoutineRepository routineRepository;
 
+    public RoutineListResponseDto getRoutines(Pageable pageable) {
+
+        Slice<Routine> routines = routineRepository.findAll(pageable)
+                .orElseThrow(NotFoundRoutine::new);
+
+        if (!routines.hasNext()) {
+            throw new NotFoundRoutine("더 이상 루틴이 존재하지 않습니다.");
+        }
+
+        List<RoutineDto> routineDtos = routines.stream()
+                .map(routine -> {
+                    return RoutineDto.builder()
+                            .id(routine.getId())
+                            .title(routine.getTitle())
+                            .author(routine.getUser().getNickname())
+                            .description(routine.getContent())
+//                            .likeCounts()
+//                            .scrapCounts()
+                            .createdAt(routine.getCreatedAt())
+                            .build();
+                })
+                .toList();
+
+        return RoutineListResponseDto.builder()
+                .routines(routineDtos)
+                .currentPage(routines.getNumber())
+                .hasNext(routines.hasNext())
+                .build();
+
+    }
 }
