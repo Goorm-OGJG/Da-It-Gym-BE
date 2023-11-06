91f415f56f3f5dd98384d42341e2565da23fedaf
ByeongGyu An <devafox62@gmail.com>
2023-11-06 13:11:10 +0900

    Fix : 루틴 관련 테이블 구조 변경 루틴은 각 Day에 운동을 정의할 수 있고, 해당 운동은 세부적으로 계획할 수 있다. 현재의 구조는 각 루틴에 하나의 운동만 포함될 수 있기 때문에 테이블의 변경이 필요하여 수정을 진행.

---
+++ src/main/java/com/ogjg/daitgym/domain/routine/ExerciseDetail.java
@@ -0,0 +1,53 @@
+package com.ogjg.daitgym.domain.routine;
+
+import com.ogjg.daitgym.domain.BaseEntity;
+import com.ogjg.daitgym.domain.Exercise;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class ExerciseDetail extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "day_id")
+    private Day day;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+
+    private int setCount;
+
+    private int repetitionCount;
+
+    private int weight;
+
+    private int order;
+
+    private LocalTime restTime;
+
+    @Builder
+    public ExerciseDetail(Day day, Exercise exercise, int setCount, int repetitionCount, int weight, int order, LocalTime restTime) {
+        this.day = day;
+        this.exercise = exercise;
+        this.setCount = setCount;
+        this.repetitionCount = repetitionCount;
+        this.weight = weight;
+        this.order = order;
+        this.restTime = restTime;
+    }
+}
