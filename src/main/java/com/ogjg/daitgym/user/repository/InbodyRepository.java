7a6f8ae1520725965411857bd1eb04c5dccdfde2
dunowljj <uuufg3322@gmail.com>
2023-11-07 12:36:31 +0900

    Feat : DIG-82 인바디 조회 기능 추가
    - GetInbodiesResponse 내부의 InbodyDetailsAvg에서 초기화와 동시에 평균 계산

--- src/main/java/com/ogjg/daitgym/user/repository/InbodyRepository.java
+++ src/main/java/com/ogjg/daitgym/user/repository/InbodyRepository.java
@@ -3,5 +3,8 @@ package com.ogjg.daitgym.user.repository;
 import com.ogjg.daitgym.domain.Inbody;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InbodyRepository extends JpaRepository<Inbody, Long> {
+    List<Inbody> findByUserEmail(String email);
 }
