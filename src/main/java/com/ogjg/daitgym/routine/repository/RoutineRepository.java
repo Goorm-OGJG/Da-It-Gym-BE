7c239ca52297f13e09e7ca253a16164afa6f587b
ByeongGyu An <devafox62@gmail.com>
2023-11-11 19:44:29 +0900

    Feat : DIG-39 루틴 삭제 기능 구현 - 루틴 삭제 기능 추가 - 삭제하려는 루틴의 사용자 본인이 아닌 경우 예외처리 적용

--- src/main/java/com/ogjg/daitgym/routine/repository/RoutineRepository.java
+++ src/main/java/com/ogjg/daitgym/routine/repository/RoutineRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface RoutineRepository extends JpaRepository<Routine, String> {
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
 
     @Query("SELECT r FROM Routine r WHERE (:division IS NULL OR r.division = :division)")
     Optional<Slice<Routine>> findAllByDivision(@Param("division") Integer division, Pageable pageable);
@@ -21,6 +21,6 @@ public interface RoutineRepository extends JpaRepository<Routine, String> {
     @Query("SELECT r FROM Routine r WHERE (:division IS NULL OR r.division = :division) AND r.user.email IN :followerEmails")
     Optional<Slice<Routine>> findByDivisionAndUserEmailIn(@Param("division") Integer division, @Param("followerEmails") List<String> followerEmails, Pageable pageable);
 
-    Optional<Routine> findById(Long routineId);
+//    Optional<Routine> findById(Long routineId);
 
 }
