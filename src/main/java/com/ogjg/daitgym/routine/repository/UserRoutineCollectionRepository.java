5a17e005aa0b8df7ef7504d1e6a197df9e62b83a
ByeongGyu An <devafox62@gmail.com>
2023-11-12 16:11:30 +0900

    Feat : DIG-36 루틴 스크랩 조회 기능 구현 - 루틴 스크랩 조회 기능 구현 - Slice를 활용한 무한 스크롤 데이터 반환

--- src/main/java/com/ogjg/daitgym/routine/repository/UserRoutineCollectionRepository.java
+++ src/main/java/com/ogjg/daitgym/routine/repository/UserRoutineCollectionRepository.java
@@ -1,9 +1,18 @@
 package com.ogjg.daitgym.routine.repository;
 
+import com.ogjg.daitgym.domain.routine.Routine;
 import com.ogjg.daitgym.domain.routine.UserRoutineCollection;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import static com.ogjg.daitgym.domain.routine.UserRoutineCollection.*;
 
 public interface UserRoutineCollectionRepository extends JpaRepository<UserRoutineCollection, PK> {
+
+    @Query("SELECT urc.routine FROM UserRoutineCollection urc WHERE urc.pk.email = :email")
+    Slice<Routine> findRoutinesByUserEmail(@Param("email") String email, Pageable pageable);
+
 }
