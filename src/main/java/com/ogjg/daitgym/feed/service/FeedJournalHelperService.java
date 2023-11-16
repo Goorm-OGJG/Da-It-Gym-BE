f5497315d4be80e8fafc004737ee60a057948447
DongJinLee <dlehdwls21@naver.com>
2023-11-16 16:01:06 +0900

    Fix : 운동일지 예외에 대한 버그 수정
    ### 일지 삭제시 피드가 존재하지않으면 예외가 발생하는 버그 수정
    ### 일지 공유시 이미 공유된 상태라면 공유할 수 없다는 예외를 발생
    ### 운동일지 생성시 해당 날짜에 일지가 존재한다면 예외 발생

--- src/main/java/com/ogjg/daitgym/feed/service/FeedJournalHelperService.java
+++ src/main/java/com/ogjg/daitgym/feed/service/FeedJournalHelperService.java
@@ -4,6 +4,7 @@ package com.ogjg.daitgym.feed.service;
 import com.ogjg.daitgym.comment.feedExerciseJournal.exception.NotFoundFeedJournal;
 import com.ogjg.daitgym.domain.User;
 import com.ogjg.daitgym.domain.feed.FeedExerciseJournal;
+import com.ogjg.daitgym.domain.journal.ExerciseJournal;
 import com.ogjg.daitgym.feed.repository.FeedExerciseJournalRepository;
 import com.ogjg.daitgym.user.exception.NotFoundUser;
 import com.ogjg.daitgym.user.repository.UserRepository;
@@ -23,6 +24,13 @@ public class FeedJournalHelperService {
     public FeedExerciseJournal findFeedJournalById(Long feedJournalId) {
         return feedExerciseJournalRepository.findById(feedJournalId)
                 .orElseThrow(NotFoundFeedJournal::new);
+    }
+
+    /**
+     * 운동일지로 피드운동일지가 존재하는지 확인하기
+     */
+    public boolean checkExistFeedExerciseJournalByExerciseJournal(ExerciseJournal exerciseJournal){
+        return feedExerciseJournalRepository.findByExerciseJournal(exerciseJournal).isPresent();
     }
 
     public User findUserByNickname(String nickname) {
