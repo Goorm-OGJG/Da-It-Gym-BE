f398b7f278aa628e1fe48c58a997dee131fe936f
YEJINGO <yaejingo@gmail.com>
2023-11-17 16:10:48 +0900

    Fix : DIG-136 모든 메세지 조회하도록 수정
    - 메세지가 100개까지만 로드됨 -> 모든 메세지가 보이도록 수정

--- src/main/java/com/ogjg/daitgym/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/ogjg/daitgym/chat/repository/ChatMessageRepository.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    List<ChatMessage> findTop100ByRedisRoomIdOrderByMessageCreatedAtAsc(String roomId);
-
     ChatMessage findTop1ByRedisRoomIdOrderByMessageCreatedAtDesc(String roomId);
 
     List<ChatMessage> findAllByRedisRoomIdAndReadCountAndSenderNot(String redisRoomId, int readCount, String nickname);
+
+    List<ChatMessage> findAllByRedisRoomIdOrderByMessageCreatedAtAsc(String roomId);
 }
