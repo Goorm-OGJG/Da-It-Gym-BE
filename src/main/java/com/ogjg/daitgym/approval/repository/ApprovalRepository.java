7d9eb40d71b52686c3066ee51342d26a254f7c72
dunowljj <uuufg3322@gmail.com>
2023-11-17 22:27:42 +0900

    Feat : DIG-137 승인 요청 목록 보기 기능 구현
    - approveStatus에 title값 추가
      - title 값은 프론트에 내려주는 값
    - 승인 요청 목록 보기 기능 구현
      - 페이징 기본 10개씩
      - 1차 정렬 기준 : 승인 대기 -> 보류 -> 트레이너 승인  -> 승인 거부 순으로 정렬
        - enum 배치 순서대로 승인상태에 따라 1차 정렬
        - Approval 입장에서 Certification 혹은 award 중 한쪽이 null 일 수 있다.
        - UNION을 사용하고 정렬과 조회를 한번에 하기위해 네이티브 쿼리를 사용했다.
      - 2차 정렬 기준 : ApprovalId 값 오름차순 (심사 요청 생성 일시 기준과 같다.)

--- src/main/java/com/ogjg/daitgym/approval/repository/ApprovalRepository.java
+++ src/main/java/com/ogjg/daitgym/approval/repository/ApprovalRepository.java
@@ -2,6 +2,31 @@ package com.ogjg.daitgym.approval.repository;
 
 import com.ogjg.daitgym.domain.Approval;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
+
+    @Query(value = """
+    SELECT * FROM
+    (
+    SELECT a.content, a.approval_manager, a.created_at , a.modified_at, a.id as id, a.approve_status as approve_status 
+        FROM Approval a
+        INNER JOIN certification c ON a.id = c.approval_id
+        INNER JOIN users u ON c.email = u.email
+        WHERE u.nickname LIKE :nickname
+    UNION
+    SELECT a.content, a.approval_manager, a.created_at , a.modified_at, a.id as id, a.approve_status as approve_status 
+        FROM Approval a
+        INNER JOIN award aw ON a.id = aw.approval_id
+        INNER JOIN users u ON aw.email = u.email
+        WHERE u.nickname LIKE :nickname
+    )
+    AS mytable ORDER BY mytable.approve_status ASC, mytable.id ASC LIMIT :limit OFFSET :offset
+    """, nativeQuery = true)
+    List<Approval> findByUserNickname(@Param("nickname") String nicknamePattern, @Param("limit") int limit, @Param("offset") int offset);
 }
+
+
