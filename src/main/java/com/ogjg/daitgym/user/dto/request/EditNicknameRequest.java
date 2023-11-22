4b25002fd410eeeaaa8f2afc6874c291b1d6e1fc
dunowljj <uuufg3322@gmail.com>
2023-11-23 04:01:55 +0900

    Feat : DIG-83 닉네임 Pattern 추가
    - 초기 uuid 닉네임을 고려하여 user 엔티티의 nickname에는 3-36 길이가 가능하도록 설정
    - 나머지 수정 요청의 nickname은 3-11 까지만 허용

--- src/main/java/com/ogjg/daitgym/user/dto/request/EditNicknameRequest.java
+++ src/main/java/com/ogjg/daitgym/user/dto/request/EditNicknameRequest.java
@@ -1,5 +1,6 @@
 package com.ogjg.daitgym.user.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 public class EditNicknameRequest {
 
+    @Pattern(regexp = "^[a-zA-Z0-9_]{3,11}$", message = "닉네임은 영문, 숫자, _ 만 사용 가능하며, 길이는 3자 이상 11자 이하여야 합니다.")
     private String nickname;
 
     @Builder
