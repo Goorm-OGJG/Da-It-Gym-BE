7636082ba63530d64828cfcaa2a8f7f048d475d2
DongJinLee <dlehdwls21@naver.com>
2023-11-24 00:39:19 +0900

    Fix DIG-160: 카카오 소셜 Exception 추가
    ### 카카오 소셜 친구목록 받아오기 테스트

--- src/main/java/com/ogjg/daitgym/user/controller/UserFriendController.java
+++ src/main/java/com/ogjg/daitgym/user/controller/UserFriendController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/kakao")
+@RequestMapping("/api/users/kakao")
 @RequiredArgsConstructor
 public class UserFriendController {
 
