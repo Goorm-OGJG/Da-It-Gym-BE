ad7de3b0a1d64a156a2437417e1be7a4b3f61168
dunowljj <uuufg3322@gmail.com>
2023-11-24 01:14:31 +0900

    Fix : DIG-143 승인 요청 목록 조회 기능 수정. 네티이브 쿼리 제거
    - UNION 쿼리를 페이징하면 중복제거 이전의 데이터로 페이징이 되어 문제 발생
    - 네이티브 쿼리를 제거하고 객체그래프로 탐색
    - Approval을 기준으로 조회하는게 아닌 User부터 nickname을 검색해 조회하도록 수정

--- src/main/java/com/ogjg/daitgym/admin/service/AdminService.java
+++ src/main/java/com/ogjg/daitgym/admin/service/AdminService.java
@@ -4,37 +4,80 @@ import com.ogjg.daitgym.admin.dto.request.EditApprovalRequest;
 import com.ogjg.daitgym.admin.dto.response.GetApprovalDetailResponse;
 import com.ogjg.daitgym.admin.dto.response.GetApprovalsResponse;
 import com.ogjg.daitgym.approval.repository.ApprovalRepository;
+import com.ogjg.daitgym.approval.repository.AwardRepository;
+import com.ogjg.daitgym.approval.repository.CertificationRepository;
 import com.ogjg.daitgym.common.exception.approval.NotFoundApproval;
 import com.ogjg.daitgym.domain.Approval;
 import com.ogjg.daitgym.domain.ApproveStatus;
 import com.ogjg.daitgym.domain.User;
+import com.ogjg.daitgym.user.repository.UserRepository;
 import com.ogjg.daitgym.user.service.UserHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
     private final ApprovalRepository approvalRepository;
-
+    private final AwardRepository awardRepository;
+    private final CertificationRepository certificationRepository;
+    private final UserRepository userRepository;
     private final UserHelper userHelper;
 
     @Transactional(readOnly = true)
     public GetApprovalsResponse getApprovals(String nickname, Pageable pageable) {
+        List<User> users = userRepository.findByNicknameStartingWith(nickname);
+        List<Approval> approvals = users.stream()
+                .map(User::getEmail)
+                .flatMap((email) -> getApprovals(email).stream())
+                .distinct().toList();
+
+        List<Approval> pagedApprovals = approvals.stream()
+                .sorted(comparing(Approval::getApproveStatus).thenComparing(Approval::getId))
+                .skip(getStartPage(pageable))
+                .limit(pageable.getPageSize())
+                .collect(toList());
+
         return GetApprovalsResponse.of(
-                calculateTotalPages(nickname, pageable),
-                approvalRepository.findByUserNickname(nickname, pageable.getPageSize(), pageable.getPageNumber())
+                calculateTotalPages(approvals.size(), pageable),
+                pagedApprovals
         );
     }
 
-    private long calculateTotalPages(String nickname, Pageable pageable) {
-        long totalCount = approvalRepository.countTotalPageByUserNickname(nickname);
+    private long calculateTotalPages(long totalCount, Pageable pageable) {
         long pageCount = totalCount / pageable.getPageSize();
         if (totalCount % pageable.getPageSize() != 0) pageCount++;
         return pageCount;
+    }
+
+    private List<Approval> getApprovals(String findEmail) {
+        List<Approval> approvals = new ArrayList<>();
+
+        approvals.addAll(
+                certificationRepository.findByUserEmail(findEmail).stream()
+                        .map((award -> award.getApproval()))
+                        .collect(toList())
+        );
+        approvals.addAll(
+                awardRepository.findByUserEmail(findEmail).stream()
+                        .map((certification -> certification.getApproval()))
+                        .collect(toList())
+        );
+
+        return approvals;
+    }
+
+    private int getStartPage(Pageable pageable) {
+        return (pageable.getPageNumber() - 1) * pageable.getPageSize();
     }
 
     @Transactional(readOnly = true)
