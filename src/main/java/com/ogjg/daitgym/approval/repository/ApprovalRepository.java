ced4d324ffdcf65f4e4ba6a8b00889b685c14c0c
dunowljj <uuufg3322@gmail.com>
2023-11-24 01:14:31 +0900

    Fix : DIG-143 승인 요청 목록 조회 기능 수정. 네티이브 쿼리 제거
    - UNION 쿼리를 페이징하면 중복제거 이전의 데이터로 페이징이 되어 문제 발생
    - 네이티브 쿼리를 제거하고 객체그래프로 탐색
    - Approval을 기준으로 조회하는게 아닌 User부터 nickname을 검색해 조회하도록 수정

--- src/main/java/com/ogjg/daitgym/approval/repository/ApprovalRepository.java
+++ src/main/java/com/ogjg/daitgym/approval/repository/ApprovalRepository.java
@@ -2,50 +2,8 @@ package com.ogjg.daitgym.approval.repository;
 
 import com.ogjg.daitgym.domain.Approval;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
-
-    @Query(value = """
-    SELECT * FROM
-    (
-    SELECT a.content, a.approval_manager, a.created_at , a.modified_at, a.id as id, a.approve_status as approve_status 
-        FROM approval a
-        INNER JOIN certification c ON a.id = c.approval_id
-        INNER JOIN users u ON c.email = u.email
-        WHERE u.nickname LIKE :nickname%
-    UNION
-    SELECT a.content, a.approval_manager, a.created_at , a.modified_at, a.id as id, a.approve_status as approve_status 
-        FROM approval a
-        INNER JOIN award aw ON a.id = aw.approval_id
-        INNER JOIN users u ON aw.email = u.email
-        WHERE u.nickname LIKE :nickname%
-    )
-    AS mytable ORDER BY mytable.approve_status ASC, mytable.id ASC LIMIT :limit OFFSET :offset
-    """, nativeQuery = true)
-    List<Approval> findByUserNickname(@Param("nickname") String nickname, @Param("limit") int limit, @Param("offset") int offset);
-
-
-    @Query(value = """
-    SELECT COUNT(*) FROM
-    (
-    SELECT a.id 
-        FROM approval a
-        INNER JOIN certification c ON a.id = c.approval_id
-        INNER JOIN users u ON c.email = u.email
-        WHERE u.nickname LIKE :nickname%
-    UNION
-    SELECT a.id 
-        FROM approval a
-        INNER JOIN award aw ON a.id = aw.approval_id
-        INNER JOIN users u ON aw.email = u.email
-        WHERE u.nickname LIKE :nickname%
-    ) AS mytable
-    """, nativeQuery = true)
-    long countTotalPageByUserNickname(@Param("nickname") String nickname);
 }
 
 
--- src/main/java/com/ogjg/daitgym/user/repository/UserRepository.java
+++ src/main/java/com/ogjg/daitgym/user/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
@@ -14,12 +15,12 @@ public interface UserRepository extends JpaRepository<User,String> {
 
     Optional<User> findByEmail(String email);
 
-
     @Query("""
     select u from User u where u.email = :email
 """)
     Optional<User> findByEmailIncludingDeleted(@Param("email") String email);
 
-
     Page<User> findByNicknameStartingWith(String nickname, Pageable pageable);
+
+    List<User> findByNicknameStartingWith(String nickname);
 }
